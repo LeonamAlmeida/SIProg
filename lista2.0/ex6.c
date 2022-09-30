ffdfdc06bcb93b0cf5f28bd31ef0b82440459e3e
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-09-30 14:38:44 -0300

    Update ex6.c

--- lista2.0/ex6.c
+++ lista2.0/ex6.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <string.h>
 
+void strinv(char str[]){
+
+    int inicio, i, n = strlen(str); //strlen retorna o tamanho de uma string
+    int j = n - 1;
+    for(i = 0; i < n/2; i ++){
+        inicio = str[i];
+        str[i] = str[j];
+        str[j] = inicio;
+        j--;
+    }  
+}
+
+int main(){
+
+    char str[51];
+    int inicio;
+
+    printf("Digite a string:\n");
+    gets(str);
+
+    strinv(str);
+
+    printf("%s", str);
+
+
+    return 0;
+}
