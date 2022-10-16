1b32ae5f778e563aec7b8334f852a0ef2ec09240
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-10-16 15:28:52 -0300

    Update ex2.c

--- lista3.0/ex2.c
+++ lista3.0/ex2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int somatorio(int n){
+
+    if(n == 0){
+        return 0;
+    }
+    else{
+        return n + somatorio(n - 1);
+    }
+}
+
+int main(){
+
+    int n;
+    int soma;
+
+    printf("Digite um numero inteiro:\n");
+    scanf("%d", &n);
+
+    soma = somatorio(n);
+    printf("Soma de 1 até %d = %d", n, soma);
+
+    return 0;
+}
