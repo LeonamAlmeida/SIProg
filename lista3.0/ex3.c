1c9a697a3988da32e8258674530e7ede01ca9c08
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-10-18 19:46:46 -0300

    Update ex3.c

--- lista3.0/ex3.c
+++ lista3.0/ex3.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void eh_multiplo(int a, int b, int *resp){
+
+    if(a % b == 0){
+        *resp = 1;
+    }
+
+    else{
+        *resp = 0;
+    }
+}
+
+int main(){
+
+    int x, y, i;
+    int *vet;
+    int *resp;
+
+    printf("Digite x:\n");
+    scanf("%d", &x);
+    printf("Digite y:\n");
+    scanf("%d", &y);
+
+    int tam = y;
+
+    vet = (int *) calloc(tam, sizeof(int));
+
+    for(i = 0; i < tam; i ++){
+        printf("Digite os %d numeros\n", tam);
+        scanf("%d", &vet[i]);
+        eh_multiplo(vet[i], x, &resp);
+        printf("resp: %d\n", resp);
+
+    }
+
+    return 0;
+}
