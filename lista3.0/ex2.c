a70ab3e2ecaa2fa909b4b917119093661529464c
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-10-19 10:53:17 -0300

    Update ex2.c

--- lista3.0/ex2.c
+++ lista3.0/ex2.c
@@ -1,3 +1,5 @@
+//Crie um programa que peça um número inteiro ao usuário e retorne a soma de todos os números de 1 até o
+//número que o usuário introduziu, ou seja: 1 + 2 + 3 + ... + n. Utilize recursividade.
 #include <stdio.h>
 
 int somatorio(int n){
