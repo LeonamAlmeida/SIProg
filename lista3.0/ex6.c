d034bdd86b685c58e4cf850a7eb563321e131f91
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-10-21 11:38:42 -0300

    Update ex6.c

--- lista3.0/ex6.c
+++ lista3.0/ex6.c
@@ -1,17 +1,9 @@
-// has no field  data % notas
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int n,i;
-
-    printf("Digite o número de discentes:\n");
-    scanf("%d", &n);
-
     typedef struct Data
     {   
-        int nascimento[3];
+        int dia, mes, ano;
     }data;
 
     typedef struct Notas
@@ -24,17 +16,26 @@ int main()
         char nome[51];
         char curso[51];
         int idade;
-        data;
-        notas;
-    }aluno[n];
+        data nascimento;
+        notas resultados;
+    }aluno;
 
-    struct Aluno teste[n];
+int main()
+{
+    int n,i;
 
-    for(i = 1; i <= n; i++)
+    printf("Digite o número de discentes:\n");
+    scanf("%d", &n);
+
+    int soma[n];
+    float media[n];
+    struct Aluno aluno[n];
+
+    for(i = 0; i < n; i++)
     {
-        printf("Digite os dados do aluno %d", i);
-        printf("Nome");
-        fflush(stdin);
+        printf("Digite os dados do aluno %d\n", i + 1);
+        printf("Nome:\n");
+        setbuf(stdin, NULL);
         gets(aluno[i].nome);
 
         printf("Curso:\n");
@@ -43,13 +44,35 @@ int main()
         printf("Idade\n");
         scanf("%d", &aluno[i].idade);
 
-        printf("Data de nascimento(dia/mes/ano)\n");
-        scanf("%d", &aluno[i].data.nascimento);
+        printf("Data de nascimento(xx/xx/xxxx)\n");
+        scanf("%d", &aluno[i].nascimento.dia);
+        scanf("%d", &aluno[i].nascimento.mes);
+        scanf("%d", &aluno[i].nascimento.ano);
 
         printf("Notas:\n");
-        scanf("%lf", &aluno[i].notas.nota);
+        scanf("%lf", &aluno[i].resultados.nota[0]);
+        scanf("%lf", &aluno[i].resultados.nota[1]);
+        scanf("%lf", &aluno[i].resultados.nota[2]);
+
+        soma[i] =  aluno[i].resultados.nota[0] + aluno[i].resultados.nota[1] + aluno[i].resultados.nota[2];
+        media[i] = (2 * aluno[i].resultados.nota[0] + 3 * aluno[i].resultados.nota[1] + 4 * aluno[i].resultados.nota[2])/9;
     }
 
+    printf("\n");
+    printf("***Info dos alunos***\n");
+    printf("\n");
+    for(i = 0; i < n; i++)
+    {   
+        printf("*Aluno %d*\n", i + 1);
+        printf("Nome: %s\n", aluno[i].nome);
+        printf("Curso: %s\n", aluno[i].curso);
+        printf("Idade: %d\n", aluno[i].idade);
+        printf("Data de nascimento\n%d/%d/%d\n", aluno[i].nascimento.dia, aluno[i].nascimento.mes, aluno[i].nascimento.ano);
+        printf("Notas:\n P1 = %lf\n P2 = %lf\n P3 = %lf\n", aluno[i].resultados.nota[0], aluno[i].resultados.nota[1], aluno[i].resultados.nota[2]);
+        printf("Media total: %.2f", media[i]);
+        printf("\n");
+        printf("\n");
+    }   
 
     return 0;
 }
