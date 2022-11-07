e07eb48354de07872852cfb3acfc827ea56f8b3a
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-11-07 11:55:29 -0300

    Create GetSystemTime.java

---
+++ javacodigo/simplestf/GetSystemTime.java
@@ -0,0 +1,12 @@
+package com.mycompany.systemtime;
+
+import java.util.Date;
+
+public class SystemTime {
+
+    public static void main(String[] args) {
+        Date time = new Date();
+        System.out.println("System time is:");
+        System.out.println(time.toString());
+    }
+}
