94abe5e09040ebe209d5ba50953e4480982836cd
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-11-07 16:06:18 -0300

    Create sum.java

---
+++ javacodigo/simplestf/sum.java
@@ -0,0 +1,9 @@
+public class Program
+{
+    public static void main(String[] args) {
+	int beer  = 64;
+    int whisky = 23;
+       //calculate the sum and output it
+       System.out.println(beer+whisky);        
+	}
+}
