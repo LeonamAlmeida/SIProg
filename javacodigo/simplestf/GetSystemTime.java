9e63d3c430aa91a75fc199b135df776b5d2155d1
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-11-07 12:09:04 -0300

    Update GetSystemTime.java

--- javacodigo/simplestf/GetSystemTime.java
+++ javacodigo/simplestf/GetSystemTime.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class SystemTime {
 
     public static void main(String[] args) {
-        Date time = new Date();
+        Date time = new Date(); /* new cria um objeto de uma determinada classe */
         System.out.println("System time is:");
         System.out.println(time.toString());
     }
