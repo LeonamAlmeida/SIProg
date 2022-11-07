a3373e1c8f78964c5fb377675db0604084ea128a
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-11-07 17:43:20 -0300

    Create factorial.java

---
+++ javacodigo/simplestf/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+            int  number = scanner.nextInt();
+            int fact = 1;
+            //your code goes here
+            while(number >= 1){
+                fact *= number;
+                number--;
+            }
+            System.out.println(fact);
+    }
+}
