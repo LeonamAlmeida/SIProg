395df038ac2560f23538940f104b6173d7932c01
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-11-08 18:07:10 -0300

    Update reverseAstring.java

--- javacodigo/simplestf/reverseAstring.java
+++ javacodigo/simplestf/reverseAstring.java
@@ -22,3 +22,23 @@ public class Program
 		System.out.println(arr);
 	}
 }
+// easier
+/* import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		char[] arr = text.toCharArray();
+		
+		//your code goes here
+		
+		int i;
+		int sup = arr.length;
+		for(i = sup - 1; i >= 0; i--){ // sup-1 because Java starts counting at 0
+			System.out.println(arr[i]);
+		}
+
+		}
+} */
