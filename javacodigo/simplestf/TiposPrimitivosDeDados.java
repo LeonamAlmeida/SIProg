b08cb33b16e66df88a51d9b41fb757ee91c6d9d8
Leonam Victor <72819215+LeonamAlmeida@users.noreply.github.com>
2022-11-09 11:01:56 -0300

    Create TiposPrimitivosDeDados.java

---
+++ javacodigo/simplestf/TiposPrimitivosDeDados.java
@@ -0,0 +1,19 @@
+public class TiposPrimitivosDeDados {
+    public static void main(String[] args){
+        
+        byte a;    // numeros pequenos de -128 a 127, 8 bits
+        short b;   // 16 bits, -32k até +32k
+        int c;     // 32 bits, -2bi até +2bi
+        long d;    // numeros gigantes
+                   // ambos os 4 armazenam valores inteiros
+        
+        float e;   // armazena numeros com uma precisão menor que o double
+        double f;  // os 2 são para valores decimais
+        
+        boolean g; // 0 ou 1, V ou F
+                   // boolean g = true, boolean g = false
+        char h;    // caractere
+                   // ex: char h = '?'
+        
+    }
+}
